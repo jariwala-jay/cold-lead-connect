36ebdcd5e8d44e45b8c6aef66227ebee6d8678e4
Kushal-V11 <kushalvakharia123@gmail.com>
2024-04-27 13:44:15 +0530

    initial form done

---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({
+      name: "",
+      email: "",
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
